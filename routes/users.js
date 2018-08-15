98e155ce23c9858589016d5b177614561a531e8b
Gabriel Domene <domenee.g@gmail.com>
2018-08-15 12:24:43 -0300

    Base route scheme

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/getNomeDisciplina', function(req, res, next) {
+  let nomeDisc = req.body.nomeDisciplina;
+  console.log(nomeDisc);
+  res.write('SALA BUSCADA PELO NOME');
+  res.end();
+});
+
+router.post('/getIdDisciplina', function(req, res, next){
+  let idDisc = req.body.IdDisciplina;
+  console.log(idDisc);
+  res.write('SALA BUSCADA PELO ID');
+  res.end();
 });
 
 module.exports = router;
