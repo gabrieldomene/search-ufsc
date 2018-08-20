7df81209674b8191faf6c3c68c40426542f3b139
Gabriel Domene <domenee.g@gmail.com>
2018-08-19 23:16:34 -0300

    Routes to search in fie

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,53 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+var rawdata = fs.readFileSync('last_file.json');
+var jsonBD = JSON.parse(rawdata);
+
+
+
 
 router.post('/getNomeDisciplina', function(req, res, next) {
-  let nomeDisc = req.body.nomeDisciplina;
-  console.log(nomeDisc);
-  res.write('SALA BUSCADA PELO NOME');
+  
+  res.write('OPÇÃO NÃO DISPONÍVEL');
   res.end();
 });
 
 router.post('/getIdDisciplina', function(req, res, next){
-  let idDisc = req.body.IdDisciplina;
-  console.log(idDisc);
-  res.write('SALA BUSCADA PELO ID');
-  res.end();
+  var idDisc = req.body.IdDisciplina;
+  /* let myReadStream = fs.createReadStream('lista-arquivo.txt', 'utf8'); */
+
+  console.log(jsonBD);
+  /* let vetor = [];
+  myReadStream.on('data', function(chunk){
+    console.log('new chunk received');
+    for(let i = 0; i < chunk.length; i++){
+      if(chunk[i] == '\\' || chunk[i] == 'n' || chunk[i] == 'r'){
+        continue
+      }else{
+        vetor.push(chunk[i]);
+      }
+    }
+  }); */
+
+
+  /* fs.open('lista-arquivo.txt', 'r', function(err, fd){
+    if (err) throw err;
+    else{
+      fs.readFile('lista-arquivo.txt', 'utf8', function(err, data){
+        if (err) throw err;
+        else{
+          let aux = []
+          aux.push(data)
+          console.log(aux);
+        }
+      });
+      fs.close(fd, (err) =>{
+        if (err) throw err;
+      });
+    }
+  }); */
+  res.render('index');
 });
 
 module.exports = router;
